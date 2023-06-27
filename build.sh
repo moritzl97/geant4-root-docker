bd1fbdbbed539bfcc528d733380fab5b47da4df7
molackne <moritz.andreas.lackner@cern.ch>
2023-06-27 14:07:54 +0200

    Docker images now contain installation of hbar sim

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "=> Building docker"
-docker build \
-    -t $IMAGE_NAME .
--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-source /usr/local/geant4/bin/geant4.sh
-source /usr/local/geant4/share/Geant4-10.5.1/geant4make/geant4make.sh
-exec "$@"
